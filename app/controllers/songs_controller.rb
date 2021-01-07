883f5663341886eb2b1e6c72eee8cc6084e3b6e1
Cloudymolecule <baires82@gmail.com>
2021-01-07 09:54:50 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,7 +25,13 @@ class SongsController < ApplicationController
   end
 
   def new
-    @song = Song.new
+    preference = Preference.find_by(id: 1)
+    if preference.allow_create_songs == true  
+      @song = Song.new
+    else
+      redirect_to songs_path
+    end
+    
   end
 
   def create
